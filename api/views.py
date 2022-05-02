44407f7fc61d21905f86333b4d8f89a655311b0e
FangyuanXie <xie_fangyuan@outlook.com>
2022-05-02 11:42:21 -0700

    35 api - add permission

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from projects.models import Project
@@ -17,13 +18,17 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getProjects(request):
+    print("USER:",request.user)
     projects=Project.objects.all()
     serializer=ProjectSerializer(projects,many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getProject(request,pk):
+    print("USER:",request.user)
     project=Project.objects.get(id=pk)
     serializer=ProjectSerializer(project,many=False)
     return Response(serializer.data)
