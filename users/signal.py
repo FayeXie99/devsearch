db21e2e081badf6cdee388029011ca6d70e4907f
FangyuanXie <xie_fangyuan@outlook.com>
2022-05-02 08:52:19 -0700

    email to new create account

--- users/signal.py
+++ users/signal.py
@@ -5,6 +5,9 @@ from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
 from .models import Profile
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 #@receiver(post_save,sender=Profile)
 def createProfile(sender,instance,created,**kwargs):
@@ -17,6 +20,16 @@ def createProfile(sender,instance,created,**kwargs):
             email=user.email,
             name=user.first_name,
         )
+        subject = 'Welcome to DEVSEARCH!'
+        message='We are glad you are here!'
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
 
 def updateUser(sender,instance,created, **kwargs):
     profile=instance
