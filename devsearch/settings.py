0ca3e2e02115d0502dfd8accad8f49705fed1184
FangyuanXie <xie_fangyuan@outlook.com>
2022-06-04 10:57:50 -0700

    deploy to new url

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c^pad2*v72r5%)2)7(oh$9jr7tmjweukp(ll4t%0d9qy8i*yce'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['localhost','127.0.0.1','https://devsearch-share-projects.herokuapp.com']
+DEBUG = False
+ALLOWED_HOSTS = ['localhost','127.0.0.1','https://devsearch-share-projects.herokuapp.com','https://devsearch-shareprojects.herokuapp.com']
 
 
 
@@ -206,6 +206,7 @@ AWS_ACCESS_KEY_ID = 'AKIARE72MN7A2JKSY5V3'
 AWS_SECRET_ACCESS_KEY = "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"
 AWS_STORAGE_BUCKET_NAME = 'devsearch-bucket002'
 AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE =False
 
 if os.getcwd() == '/app':
     DEBUG=False
